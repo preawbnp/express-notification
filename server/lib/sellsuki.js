20e09b380095b8421d01acb7f6d0b88fb20690b0
Mildnchrt <Nutcharueta.s@ku.th>
2018-06-26 16:43:51 +0700

    Fixed error eslint in lib/sellsuki

--- server/lib/sellsuki.js
+++ server/lib/sellsuki.js
@@ -1,13 +1,15 @@
+const axios = require('axios')
+
 function getSellsukiData (storeId) {
   return new Promise((resolve, reject) => {
     resolve(
       axios.get('http://192.168.1.254:8003/store/get-store-notification?store_ids[]=' + storeId)
-      .then(function (response) {
-        return response
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
+        .then(function (response) {
+          return response
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
     )
   })
 }
