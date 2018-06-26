8d6794a9a403a6d9e301a9a79e4fd26d0e03c123
preawbnp <preawbnp@preawbnps-MBP.localdomain>
2018-06-26 16:35:34 +0700

    Fixed coding style by esLint in lib/onesignal

--- server/lib/onesignal.js
+++ server/lib/onesignal.js
@@ -1,42 +1,42 @@
-function getLanguage(playerId) {
+function getLanguage (playerId) {
 
 }
 
-var sendNotification = function(message) {
+var sendNotification = function (message) {
   var headers = {
-    "Content-Type": "application/json; charset=utf-8",
-    "Authorization": "Basic ZjhhZjViNjYtYmUwZS00Zjg0LTk3NmQtYzQ1ZmM4ZDVhOGI2"
-  };
-  
+    'Content-Type': 'application/json; charset=utf-8',
+    'Authorization': 'Basic ZjhhZjViNjYtYmUwZS00Zjg0LTk3NmQtYzQ1ZmM4ZDVhOGI2'
+  }
+
   var options = {
-    host: "onesignal.com",
+    host: 'onesignal.com',
     port: 443,
-    path: "/api/v1/notifications",
-    method: "POST",
+    path: '/api/v1/notifications',
+    method: 'POST',
     headers: headers
-  };
-  
+  }
+
   var https = require('https')
-  var req = https.request(options, function(res) {  
-    res.on('message', function(message) {
-      console.log("Response:")
+  var req = https.request(options, function (res) {
+    res.on('message', function (message) {
+      console.log('Response:')
       console.log(JSON.parse(message))
     })
   })
-  
-  req.on('error', function(e) {
-    console.log("ERROR:")
+
+  req.on('error', function (e) {
+    console.log('ERROR:')
     console.log(e)
   })
-  
+
   req.write(JSON.stringify(message))
   req.end()
 }
 
 function getPlayerId () {
   var playerId
-  OneSignal.push(async function() {
-    OneSignal.getUserId(async function(userId) {
+  OneSignal.push(async function () {
+    OneSignal.getUserId(async function (userId) {
       playerId = userId
     })
   })
